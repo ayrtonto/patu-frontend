f70148aa958d8fe2238310d0f6179b4d7500f170
Ayrton Jafet Samaniego Millan <u202122788@upc.edu.pe>
2026-05-16 21:40:00 -0500

    Fix: login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}auth/login`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
